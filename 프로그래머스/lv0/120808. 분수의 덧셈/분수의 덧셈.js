d6bf27c24ef2497e43b9170b8e6581092bfd96b5
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2023-08-08 17:09:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 2.80 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,39 @@
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
+    let min = 0;
+    let num = 0;
+    
+    (denom1 > denom2) ? min = denom2 : min = denom1;
+    
+    for(i=1; i<=min; i++){
+        if((denom1%i == 0) && (denom2%i == 0)){
+            num = i;
+        }
+    }
+    
+    let denom_cal = num * (denom1/num) * (denom2/num);
+    let numer_cal = (denom_cal/denom1 * numer1) + (denom_cal/denom2 * numer2);
+    
+    //약분
+    num = 0;
+    
+    (denom_cal > numer_cal) ? min = numer_cal : min = denom_cal;
+    
+    for(i=1; i<=min; i++){
+        if((denom_cal%i == 0) && (numer_cal%i == 0)){
+            num = i;
+        }
+    }
+    
+    if(num === 1){
+        answer[0] = numer_cal;
+        answer[1] = denom_cal;
+    } else {
+        answer[0] = numer_cal/num;
+        answer[1] = denom_cal/num;
+    }
+    
+    console.log(num)
+    console.log(min)
+    return answer;
+}
