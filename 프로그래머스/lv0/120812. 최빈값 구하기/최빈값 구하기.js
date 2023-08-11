3e41dff41cd85a68dae8ec107b9bfb7361fee404
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2023-08-11 10:07:55 +0900

    [level 0] Title: 최빈값 구하기, Time: 2.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,37 +1,37 @@
 function solution(array) {
     var answer = 0;
-    let num = [];
+    let num_cnt = [];
     
     if(array.length === 1){
         answer = array[0];
     } else {
         //0으로 초기화
         for (i = 0; i < Math.max(...array) ; ++i) {
-            num[i] = 0;
+            num_cnt[i] = 0;
         }
 
         //num의 index위치가 array에 있는 원소의 값
         //원소의 개수는 해당 index의 값
         //ex. array = [1,1,2] -> num = [0,2,1] : 1이 2개, 2가 1개이므로 num의 index 1번위치의 값이 2, 2번위치의 값이 1
         for(i=0; i<array.length; i++){
-            if(!!num[array[i]]){
-                num[array[i]]++;
+            if(!!num_cnt[array[i]]){
+                num_cnt[array[i]]++;
             } else {
-                num[array[i]] = 1;
+                num_cnt[array[i]] = 1;
             }
         }
         console.log(`array: ${array}`)
-        console.log(`num: ${num}`)
+        console.log(`num_cnt: ${num_cnt}`)
 
         //최대로 반복된 횟수
-        let max = Math.max(...num);
+        let max = Math.max(...num_cnt);
 
         //최빈값이 중복인지 확인
-        let count = num.filter(v => max === v).length;
+        let count = num_cnt.filter(v => max === v).length;
         console.log(`최빈값의 개수: ${count}`)
 
         if(count === 1){
-            answer = num.indexOf(max);
+            answer = num_cnt.indexOf(max);
         } else {
             //최빈값이 중복이라면
             answer = -1;
