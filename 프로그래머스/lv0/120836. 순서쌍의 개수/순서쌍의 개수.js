32de39907fbcfc9d0acf7e5a3bcdd73506b88d55
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2023-08-15 22:05:28 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.34 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    let cnt = 0;
+    
+    for(i=1; i<=n; i++){
+        if(n%i === 0){
+            cnt++
+        }
+    }
+    
+    answer = cnt;
+    
+    return answer;
+}
