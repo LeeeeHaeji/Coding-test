95103a1b923ef70caa3613b6412a32f9257a0c1f
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2024-02-24 22:22:48 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,19 +1,21 @@
 function solution(arr) {
     var answer = 0;
     let divide_nums = [];
-    let cnt = 2;
     let is_divide = false;
-    
+    let cnt = 2;
+      
     while(cnt <= Math.max(...arr)){
         for(i=0; i<arr.length; i++){
             for(j=i+1; j<arr.length; j++){
                 if((arr[i]%cnt === 0) && (arr[j]%cnt === 0)){
+                    // 공통으로 나누어지는 수가 있는지 확인
                     is_divide = true;
                 }
             }
         }
         
         if(is_divide){
+            // 나눌 수 있는 수가 있다면
             for(i=0; i<arr.length; i++){
                 if(arr[i]%cnt === 0){
                     arr[i] = arr[i]/cnt;
@@ -27,6 +29,7 @@ function solution(arr) {
     }
 
     if(divide_nums[0] === undefined){
+        // 나누어지는 수가 하나도 없다면
         answer = arr.reduce((acc, cur) => {
             return acc * cur
         })
