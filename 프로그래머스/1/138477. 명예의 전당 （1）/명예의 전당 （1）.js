b2e3cc133ffcf434800e5c9b0e2d75ed16261770
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2024-03-06 23:28:11 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,4 +1,41 @@
 function solution(k, score) {
     var answer = [];
+    let topList = [];
+    /*
+    let showScore = new Array(score.length).fill(0);
+    let cnt = 0;
+
+    while(cnt <= score.length){
+        if(topList.length < k){
+            topList.push(score[cnt]);
+            topList.sort((a,b) => b-a);
+            showScore[cnt] = topList[topList.length-1];
+            
+            cnt++;
+        } else{
+            if(topList[k-1] < score[cnt]){
+                topList[k-1] = score[cnt];
+                topList.sort((a,b) => b-a);
+                showScore[cnt] = topList[k-1];
+            }else {
+                showScore[cnt] = topList[k-1];
+            }
+            cnt++;
+        }
+    }
+    showScore.pop();
+    */
+    
+    for(i=0; i<score.length; i++){
+        topList.push(score[i]);
+        topList.sort((a,b) => b-a);
+        
+        if(topList.length < k){
+            answer.push(topList[topList.length-1]);
+        } else{
+            answer.push(topList[k-1]);
+        }
+    }
+    
     return answer;
 }
