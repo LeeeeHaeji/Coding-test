a32d0b3d6c473b88a03f7cf592ed527343c00f04
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2025-01-18 19:43:12 +0900

    [level 1] Title: 카드 뭉치, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,47 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    let stack = [];
+    
+    for(i=goal.length-1; i>=0; i--){
+        stack.push(goal[i]);
+    }
+
+    while(stack.length > 0){
+        if((cards1.length > 0) && (cards2.length > 0)){
+            if(stack[stack.length-1] === cards1[0]){
+                stack.pop();
+                cards1.shift();
+            } else if(stack[stack.length-1] === cards2[0]){
+                stack.pop();
+                cards2.shift();
+            }   
+        } else if(cards1.length === 0){
+            if(stack[stack.length-1] !== cards2[0]){
+                return "No";
+            } else {
+                for(i=0; i<cards2.length; i++){
+                    if(stack[stack.length-1] === cards2[0]){
+                        stack.pop();
+                        cards2.shift();
+                    }
+                }
+            }
+        } else if(cards2.length === 0){
+            if(stack[stack.length-1] !== cards1[0]){
+                return "No";
+            }else {
+                for(i=0; i<cards1.length; i++){
+                    if(stack[stack.length-1] === cards1[0]){
+                        stack.pop();
+                        cards1.shift();
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    (stack.length === 0) ? answer = "Yes" : answer = "No"
+
+    return answer;
+}
