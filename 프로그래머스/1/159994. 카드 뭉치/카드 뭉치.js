f452380cb641ae617d8f11ab4f52930098288ead
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2025-01-23 20:14:16 +0900

    [level 1] Title: 카드 뭉치, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,5 +1,4 @@
 function solution(cards1, cards2, goal) {
-    var answer = '';
     let stack = [];
     
     for(i=goal.length-1; i>=0; i--){
@@ -37,9 +36,47 @@ function solution(cards1, cards2, goal) {
                     }
                 }
             }
-            
         }
     }
 
     return "Yes";
+    
+    /*
+    let cards1_index = [],
+        cards2_index = [];
+    
+    let cnt1 = 0,
+        cnt2 = 0;
+    
+    for(i=0; i<goal.length; i++){
+        for(k=0; k<cards1.length; k++) {
+             if(goal[i] === cards1[k]) {
+                 cards1_index[cnt1] = k;
+                 cnt1++;
+             } 
+        }
+        for(j=0; j<cards2.length; j++) {
+            if(goal[i] === cards2[j]) {
+                cards2_index[cnt2] = j;
+                cnt2++;
+             }
+        }
+    }
+    console.log(`cards1_index : ${cards1_index}`);
+    console.log(`cards2_index : ${cards2_index}`);
+    
+    for(i=0; i<cards1_index.length; i++) {
+        if(cards1_index[i] !== i){
+            return "No"
+        }
+    }
+    
+    for(i=0; i<cards2_index.length; i++) {
+        if(cards2_index[i] !== i){
+            return "No"
+        }
+    }
+    
+    return "Yes";
+    */
 }
