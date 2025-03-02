4bb48d923d2d20e9e8ab313b9f664a73593f98e1
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2025-03-02 12:42:02 +0900

    [level 1] Title: 기사단원의 무기, Time: 8920.07 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,4 +1,5 @@
 function solution(number, limit, power) {
+    /*
     var answer = 1;
     let cnt = 0;
     
@@ -21,6 +22,30 @@ function solution(number, limit, power) {
             answer += cnt;
         }
     }
+    */
+    let list = new Array(number).fill(1); //약수의 개수 리스트
+    let sum = 0;
     
-    return answer;
+    // 약수의 개수 구하기
+    for(i=1; i<=number; i++){
+        // console.log(`i: ${i}`)
+        for(j=1; j<=i/2; j++){
+            // console.log(`j: ${j}`)
+            if(i%j === 0){
+                list[i-1]++;
+            }
+            // console.log('-------------------')
+        }
+    }
+
+    //리스트의 값들이 공격력 제한 수치를 넘는지 확인
+    list.forEach(v => {
+        if(v > limit){
+            sum += power
+        } else {
+            sum += v
+        }
+    })
+
+    return sum;
 }
