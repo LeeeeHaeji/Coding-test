69f638203c430bd9d4e9bc8678081cefe1470a29
LeeeeHaeji <116999139+LeeeeHaeji@users.noreply.github.com>
2025-03-08 23:12:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 37.97 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,18 +1,18 @@
 function solution(ingredient) {
     var answer = 0;
     let stack = [];
-
+    
     for(i=0; i<ingredient.length; i++){
         stack.push(ingredient[i]);
-        // console.log('stack:' + stack)
-        if((stack[stack.length-1] === 1) && (stack[stack.length-2] === 3)){
-            if((stack[stack.length-3] === 2) && (stack[stack.length-4] === 1)){
-                for(j=0; j<4; j++){
-                    stack.pop();
-                }
-                answer++;
+        let length = stack.length;
+        
+        if((stack[length-1] == 1) && (stack[length-2] === 3) && (stack[length-3] === 2) && (stack[length-4] == 1 )){
+            for(j=0; j<4; j++){
+                stack.pop();
             }
+            answer++;
         }
     }
+
     return answer;
 }
